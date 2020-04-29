0298bc862cef82adf756a5d2c5b5b48920b84009
Alexander Rakoczy <alex@golang.org>
2020-04-29 17:52:34 -0400

    sandbox: fix missing timezone data
    The gvisor sandbox containers were missing tzinfo, as the busybox image
    does not include it.
    
    Updates golang/go#38727
    
    Change-Id: Idc2c705e3ee31de1411507bb3abdf1afb28eeda9
    Reviewed-on: https://go-review.googlesource.com/c/playground/+/231057
    Run-TryBot: Alexander Rakoczy <alex@golang.org>
    Reviewed-by: Andrew Bonventre <andybons@golang.org>
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- tests.go
+++ tests.go
@@ -570,4 +570,29 @@ func main() {
 	<-c
 }
 `, want: "timeout running program"},
+	{
+		name: "timezone_info_exists",
+		prog: `
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	loc, _ := time.LoadLocation("Europe/Berlin")
+
+	// This will look for the name CEST in the Europe/Berlin time zone.
+	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
+	t, _ := time.ParseInLocation(longForm, "Jul 9, 2012 at 5:02am (CEST)", loc)
+	fmt.Println(t)
+
+	// Note: without explicit zone, returns time in given location.
+	const shortForm = "2006-Jan-02"
+	t, _ = time.ParseInLocation(shortForm, "2012-Jul-09", loc)
+	fmt.Println(t)
+
+}
+`, want: "2012-07-09 05:02:00 +0200 CEST\n2012-07-09 00:00:00 +0200 CEST\n"},
 }
