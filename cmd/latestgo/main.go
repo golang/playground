69a742107761100be87b1a0c06723e2c6abe1c4f
Robert Findley <rfindley@google.com>
2021-11-30 09:13:25 -0500

    playground: add a goprevplay service, hosting the previous go release
    Change-Id: Ibdd5f45062960cd09965df63c12cde394dca1f8b
    Reviewed-on: https://go-review.googlesource.com/c/playground/+/367814
    Trust: Robert Findley <rfindley@google.com>
    Run-TryBot: Robert Findley <rfindley@google.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Alexander Rakoczy <alex@golang.org>

--- cmd/latestgo/main.go
+++ cmd/latestgo/main.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 
@@ -14,9 +15,13 @@ import (
 	grpc "grpc.go4.org"
 )
 
+var prev = flag.Bool("prev", false, "whether to query the previous Go release, rather than the last (e.g. 1.17 versus 1.18)")
+
 const maintnerURI = "https://maintner.golang.org"
 
 func main() {
+	flag.Parse()
+
 	conn, err := grpc.NewClient(nil, maintnerURI)
 	if err != nil {
 		log.Fatalf("error creating grpc client for %q: %v", maintnerURI, err)
@@ -27,6 +32,10 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	idx := 0
+	if *prev {
+		idx = 1
+	}
 	// On success, the maintner API always returns at least two releases.
-	fmt.Print(resp.GetReleases()[0].GetTagName())
+	fmt.Print(resp.GetReleases()[idx].GetTagName())
 }
